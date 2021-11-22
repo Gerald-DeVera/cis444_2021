338f0258af20488ff182b6ec7d598ed0388604d2
Gerald De Vera <devergerald@gmail.com>
2021-11-21 23:34:54 -0800

    does this work?

--- flask_jwt_rest_server/secure_calls/buy_book.py
+++ flask_jwt_rest_server/secure_calls/buy_book.py
@@ -1,5 +1,6 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+from hello_flask.app import JWT_SECRET
 from tools.token_tools import create_token
 import datetime
 from tools.connect_db import purchase
@@ -10,6 +11,10 @@ def handle_request():
     logger.debug("buy_book Handle Request")
     bookName = request.form['book_id']
     time = datetime.datetime.now()
+    user = request.form["user"]
+    user = user.decode(user, JWT_SECRET, algorithms=["HS256"])
+    user = user.get('user')
 
+    logger.debug(user)
     
 
